410719344137dee8e16bf787340b4eb6a26e12f0
juanpablow <juan.santana.macedo10@outlook.com>
2024-07-08 20:58:36 -0300

    feat: add resource `users`

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule],
+  controllers: [UsersController],
+  providers: [UsersService],
 })
 export class AppModule {}
