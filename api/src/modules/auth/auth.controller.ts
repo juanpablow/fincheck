9841b6a7353098d2259690a8bed7e1ae59af9b6c
juanpablow <juan.santana.macedo10@outlook.com>
2024-07-14 00:59:07 -0300

    feat: add `AuthController` with POST `/auth/signup` and `/auth/signin` endpoint

---
+++ api/src/modules/auth/auth.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SigninDto } from './dto/signin.dto';
+import { SignupDto } from './dto/signup.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+  @Post('signin')
+  authenticate(@Body() signinDto: SigninDto) {
+    return this.authService.signin(signinDto);
+  }
+
+  @Post('signup')
+  createUser(@Body() signupDto: SignupDto) {
+    return this.authService.signup(signupDto);
+  }
+}
