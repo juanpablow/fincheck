6b0b2d8d03c61566ffad52b20146dc68483ea241
juanpablow <juan.santana.macedo10@outlook.com>
2024-07-14 01:02:11 -0300

    feat: add `AuthGuard` and configure globally in `AppModule`

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { DatabaseModule } from './infra/database/database.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './modules/auth/auth.guard';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, DatabaseModule, AuthModule],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
