229c3dcd3c9c49eab833e3d25203cbcfd546c729
juanpablow <juan.santana.macedo10@outlook.com>
2024-07-14 22:58:13 -0300

    chore: add `dotenv/config`

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
-import { DatabaseModule } from './infra/database/database.module';
-import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './modules/auth/auth.guard';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, AuthModule],
+  imports: [UsersModule, AuthModule],
   providers: [
     {
       provide: APP_GUARD,
