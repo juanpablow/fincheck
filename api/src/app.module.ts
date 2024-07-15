4eecd1637649e142ff257864e357ac2c539b2f8d
juanpablow <juan.santana.macedo10@outlook.com>
2024-07-15 13:53:26 -0300

    feat: add `CategoriesModule` to imports

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,9 +4,10 @@ import { APP_GUARD } from '@nestjs/core';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { AuthGuard } from './modules/auth/auth.guard';
+import { CategoriesModule } from './modules/categories/categories.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, CategoriesModule],
   providers: [
     {
       provide: APP_GUARD,
