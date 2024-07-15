a615938482c0c95700e0036c86d67319e5134d84
juanpablow <juan.santana.macedo10@outlook.com>
2024-07-15 17:52:47 -0300

    chore: readability

--- api/src/infra/database/repositories/categories.repositories.ts
+++ api/src/infra/database/repositories/categories.repositories.ts
@@ -6,6 +6,7 @@ import { PrismaService } from '../prisma.service';
 @Injectable()
 export class CategoriesRepository {
   constructor(private readonly prismaService: PrismaService) {}
+
   findMany(findManyDto: Prisma.CategoryFindManyArgs) {
     return this.prismaService.category.findMany(findManyDto);
   }
