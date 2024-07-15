55ac962283d19c7a7ba6b5d937e3b981a48dcbc8
juanpablow <juan.santana.macedo10@outlook.com>
2024-07-14 22:46:08 -0300

    feat: add `IsPublic` custom decorator

--- api/src/modules/auth/auth.controller.ts
+++ api/src/modules/auth/auth.controller.ts
@@ -2,17 +2,20 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SigninDto } from './dto/signin.dto';
 import { SignupDto } from './dto/signup.dto';
+import { IsPublic } from 'src/shared/decorators/IsPublic';
 
+@IsPublic()
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-  @Post('signin')
-  authenticate(@Body() signinDto: SigninDto) {
-    return this.authService.signin(signinDto);
-  }
 
   @Post('signup')
-  createUser(@Body() signupDto: SignupDto) {
+  signup(@Body() signupDto: SignupDto) {
     return this.authService.signup(signupDto);
+  }
+
+  @Post('signin')
+  signin(@Body() signinDto: SigninDto) {
+    return this.authService.signin(signinDto);
   }
 }
