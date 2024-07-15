cdb71f7a0ee2a18be1c77fe2f427eaed8d1bd648
juanpablow <juan.santana.macedo10@outlook.com>
2024-07-14 22:53:00 -0300

    refactor: return only name and email in `getUserById`

--- api/src/modules/users/users.service.ts
+++ api/src/modules/users/users.service.ts
@@ -1,59 +1,23 @@
-import { ConflictException, Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
+import { Injectable } from '@nestjs/common';
 import { UsersRepository } from 'src/infra/database/repositories/users.repositories';
-import { hash } from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepo: UsersRepository) {}
 
-  async createUser(createUserDto: CreateUserDto) {
-    const { name, email, password } = createUserDto;
-
-    const emailTaken = await this.usersRepo.findByEmail(email);
-
-    if (emailTaken) {
-      throw new ConflictException('This email is already in use');
-    }
-
-    const hashedPassword = await hash(password, 12);
-
-    const result = await this.usersRepo.createUser({
-      data: {
-        name,
-        email,
-        password: hashedPassword,
-        categories: {
-          createMany: {
-            data: [
-              // Income
-              { name: 'Salário', icon: 'salary', type: 'income' },
-              { name: 'Freelance', icon: 'freelance', type: 'income' },
-              { name: 'Outro', icon: 'other', type: 'income' },
-              // Expense
-              { name: 'Casa', icon: 'home', type: 'expense' },
-              { name: 'Alimentação', icon: 'food', type: 'expense' },
-              { name: 'Educação', icon: 'education', type: 'expense' },
-              { name: 'Lazer', icon: 'fun', type: 'expense' },
-              { name: 'Mercado', icon: 'grocery', type: 'expense' },
-              { name: 'Roupas', icon: 'clothes', type: 'expense' },
-              { name: 'Transporte', icon: 'transport', type: 'expense' },
-              { name: 'Viagem', icon: 'travel', type: 'expense' },
-              { name: 'Outro', icon: 'other', type: 'expense' },
-            ],
-          },
-        },
+  getUserById(userId: string) {
+    return this.usersRepo.findUnique({
+      where: {
+        id: userId,
+      },
+      select: {
+        name: true,
+        email: true,
       },
     });
-
-    return result;
   }
 
-  async deleteUser(userId: string) {
-    await this.usersRepo.deleteUser(userId);
-  }
-
-  async findUserById(userId: string) {
-    await this.usersRepo.findById(userId);
+  deleteUser(userId: string) {
+    return this.usersRepo.deleteUser(userId);
   }
 }
