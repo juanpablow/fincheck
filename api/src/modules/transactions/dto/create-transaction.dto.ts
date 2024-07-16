d7761ea3d0e01ef30dc5172246916cb18e03c9d3
juanpablow <juan.santana.macedo10@outlook.com>
2024-07-16 16:12:15 -0300

    feat: add `CreateTransactionDto` and `UpdateTransactionDto`

---
+++ api/src/modules/transactions/dto/create-transaction.dto.ts
@@ -0,0 +1,39 @@
+import {
+  IsDateString,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  IsUUID,
+} from 'class-validator';
+import { TransactionType } from '../entities/transaction.enum';
+
+export class CreateTransactionDto {
+  @IsString()
+  @IsUUID()
+  @IsNotEmpty()
+  bankAccountId: string;
+
+  @IsString()
+  @IsUUID()
+  @IsNotEmpty()
+  categoryId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
+  value: number;
+
+  @IsDateString()
+  @IsNotEmpty()
+  date: string;
+
+  @IsEnum(TransactionType)
+  @IsNotEmpty()
+  type: TransactionType;
+}
