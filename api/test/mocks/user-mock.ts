048bc320eb68c77afee2b5b52d681d496432573a
juanpablow <juan.santana.macedo10@outlook.com>
2024-07-17 17:56:57 -0300

    chore: initialize `Vite` in the project

--- api/test/mocks/user-mock.ts
+++
@@ -1,7 +0,0 @@
-import { SignupDto } from 'src/modules/auth/dto/signup.dto';
-
-export const mockCreateUserDto: SignupDto = {
-  name: 'Juan Pablo',
-  email: `teste-${Date.now()}@example.com`,
-  password: 'test123',
-};
