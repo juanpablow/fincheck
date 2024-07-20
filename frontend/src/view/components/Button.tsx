161adba9a42c7d8ab21dc9c69630fdfeb8e4b784
juanpablow <juan.santana.macedo10@outlook.com>
2024-07-20 00:00:56 -0300

    feat: add loading spinner to `button`
    - Improve user feedback

--- frontend/src/view/components/Button.tsx
+++ frontend/src/view/components/Button.tsx
@@ -1,12 +1,29 @@
 import { ComponentProps } from "react";
+import { cn } from "../../app/utils/cn";
+import { Spinner } from "./Spinner";
 
-interface ButtonProps extends ComponentProps<"button"> {}
+interface ButtonProps extends ComponentProps<"button"> {
+  isLoading?: boolean;
+}
 
-export function Button(props: ButtonProps) {
+export function Button({
+  className,
+  isLoading,
+  disabled,
+  children,
+  ...props
+}: ButtonProps) {
   return (
     <button
       {...props}
-      className="bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 disabled:cursor-not-allowed disabled:text-gray-400 px-6 h-12 rounded-2xl font-medium text-white transition-all"
-    />
+      disabled={disabled || isLoading}
+      className={cn(
+        "bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 disabled:cursor-not-allowed disabled:text-gray-400 px-6 h-12 rounded-2xl font-medium text-white transition-all flex items-center justify-center ",
+        className
+      )}
+    >
+      {!isLoading && children}
+      {isLoading && <Spinner className="w-6 h-6" />}
+    </button>
   );
 }
