70ffc3998f7275bf0c24db025c7c40d884cffdb4
juanpablow <juan.santana.macedo10@outlook.com>
2024-07-20 00:23:58 -0300

    feat: setup providers in `App`
    - Import `QueryClientProvider`, `AuthProvider` and `Toaster` in App component
    - Wrap all with `QueryClientProvider` and `AuthProvider`

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Router } from "./Router";
 import { Toaster } from "react-hot-toast";
+
+import { Router } from "./Router";
 import { AuthProvider } from "./app/contexts/AuthContext";
 
 const queryClient = new QueryClient();
