03d1b78602c579a94cf5854f9214ac0f81300df4
juanpablow <juan.santana.macedo10@outlook.com>
2024-07-20 00:13:44 -0300

    feat: add `signin` and `signup` functions with HTTP POST request

---
+++ frontend/src/app/services/authService/signin.ts
@@ -0,0 +1,19 @@
+import { sleep } from "../../utils/sleep";
+import { httpClient } from "../httpClient";
+
+export interface SigninParams {
+  email: string;
+  password: string;
+}
+
+interface SigninResponse {
+  accessToken: string;
+}
+
+export async function signin(body: SigninParams) {
+  await sleep();
+
+  const { data } = await httpClient.post<SigninResponse>("/auth/signin", body);
+
+  return data;
+}
---
+++ frontend/src/app/services/authService/signup.ts
@@ -0,0 +1,20 @@
+import { sleep } from "../../utils/sleep";
+import { httpClient } from "../httpClient";
+
+export interface SignupParams {
+  name: string;
+  email: string;
+  password: string;
+}
+
+interface SignupResponse {
+  accessToken: string;
+}
+
+export async function signup(body: SignupParams) {
+  await sleep();
+
+  const { data } = await httpClient.post<SignupResponse>("/auth/signup", body);
+
+  return data;
+}
