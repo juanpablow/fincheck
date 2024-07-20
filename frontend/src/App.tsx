554f1c6f818a682bf075ba9f5852a416472b14c2
juanpablow <juan.santana.macedo10@outlook.com>
2024-07-20 00:18:57 -0300

    feat: create `AuthContext` and `useAuth` hook
    - Provide authentication state through the context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,17 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Router } from "./Router";
+import { Toaster } from "react-hot-toast";
+import { AuthProvider } from "./app/contexts/AuthContext";
+
+const queryClient = new QueryClient();
 
 export function App() {
-  return <Router />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Router />;
+        <Toaster />
+      </AuthProvider>
+    </QueryClientProvider>
+  );
 }
