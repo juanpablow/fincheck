df30ede2a24c1286fd438405ef38cd1ccd055436
juanpablow <juan.santana.macedo10@outlook.com>
2024-07-21 14:48:44 -0300

    feat: add `Dashboard` page

---
+++ frontend/src/view/pages/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import { useAuth } from "../../../app/hooks/useAuth";
+import { Button } from "../../components/Button";
+
+export function Dashboard() {
+  const { signout } = useAuth();
+
+  return (
+    <>
+      <h1>Dashboard page</h1>
+      <Button onClick={signout}>Sair da conta</Button>
+    </>
+  );
+}
