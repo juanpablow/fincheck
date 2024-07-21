91de71e8cef9ea69d0070c528d2f0634f71338e3
juanpablow <juan.santana.macedo10@outlook.com>
2024-07-20 23:03:37 -0300

    chore: set `retry` and `refetchOnWindowFocus` to false

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,13 +4,20 @@ import { Toaster } from "react-hot-toast";
 import { Router } from "./Router";
 import { AuthProvider } from "./app/contexts/AuthContext";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
-        <Router />;
+        <Router />
         <Toaster />
       </AuthProvider>
     </QueryClientProvider>
