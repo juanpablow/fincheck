3f3ab4734f56e8857acee0fbf522c139ca2bc1b2
juanpablow <juan.santana.macedo10@outlook.com>
2024-07-22 00:00:03 -0300

    feat: add all icons

---
+++ frontend/src/view/components/icons/BankAccountTypeIcon/iconsMap.ts
@@ -0,0 +1,9 @@
+import { CashIcon } from "./CashIcon";
+import { CheckingIcon } from "./CheckingIcon";
+import { InvestmentIcon } from "./InvestmentIcon";
+
+export const iconsMap = {
+  checking: CheckingIcon,
+  investment: InvestmentIcon,
+  cash: CashIcon,
+};
