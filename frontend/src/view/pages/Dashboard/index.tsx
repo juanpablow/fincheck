28eefe8ae395a173f4fa892df785e05500cd6799
juanpablow <juan.santana.macedo10@outlook.com>
2024-07-23 23:02:00 -0300

    feat: wrap `Dashboard` with `DashboardProvider` - Wrap 'Dashboard' component with 'DashboardProvider' for context management - Ensure that all child components of 'Dashboard' have access to the 'DashboardContext'

--- frontend/src/view/pages/Dashboard/index.tsx
+++ frontend/src/view/pages/Dashboard/index.tsx
@@ -1,24 +1,28 @@
-import { Logo } from "../../components/Logo";
-import { UserMenu } from "../../components/UserMenu";
+import { Logo } from "@view/components/Logo";
+import { UserMenu } from "@view/components/UserMenu";
 import { Accounts } from "./components/Accounts";
 import { Transactions } from "./components/Transactions";
+import { DashboardProvider } from "./components/DashboardContext";
 
 export function Dashboard() {
   return (
-    <div className="w-full h-full p-4 md:pt-6 md:px-8 md:pb-8 flex flex-col gap-4">
-      <header className="h-12 flex items-center justify-between">
-        <Logo className="h-6 text-teal-900" />
-        <UserMenu />
-      </header>
-      <main className="flex-1 flex flex-col md:flex-row gap-4">
-        <div className="w-full md:w-1/2">
-          <Accounts />
-        </div>
+    <DashboardProvider>
+      <div className="w-full h-full p-4 md:pt-6 md:px-8 md:pb-8 flex flex-col gap-4">
+        <header className="h-12 flex items-center justify-between">
+          <Logo className="h-6 text-teal-900" />
+          <UserMenu />
+        </header>
 
-        <div className="w-full md:w-1/2">
-          <Transactions />
-        </div>
-      </main>
-    </div>
+        <main className="flex-1 flex flex-col md:flex-row gap-4 max-h-full">
+          <div className="w-full md:w-1/2">
+            <Accounts />
+          </div>
+
+          <div className="w-full md:w-1/2">
+            <Transactions />
+          </div>
+        </main>
+      </div>
+    </DashboardProvider>
   );
 }
