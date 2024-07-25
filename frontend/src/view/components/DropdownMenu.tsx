34102c1760ee5b8f3a61dd65b0d619f9108c6ffe
juanpablow <juan.santana.macedo10@outlook.com>
2024-07-25 01:45:23 -0300

    feat: add `DropdownMenu` component

---
+++ frontend/src/view/components/DropdownMenu.tsx
@@ -0,0 +1,71 @@
+/* eslint-disable react-refresh/only-export-components */
+import { cn } from "@app/utils/cn";
+import * as RdxDropdownMenu from "@radix-ui/react-dropdown-menu";
+
+function DropdownMenuRoot({ children }: { children: React.ReactNode }) {
+  return <RdxDropdownMenu.Root>{children}</RdxDropdownMenu.Root>;
+}
+
+function DropdownMenuTrigger({ children }: { children: React.ReactNode }) {
+  return (
+    <RdxDropdownMenu.Trigger className="outline-none cursor-pointer" asChild>
+      {children}
+    </RdxDropdownMenu.Trigger>
+  );
+}
+
+interface DropdownMenuContentProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+function DropdownMenuContent({
+  children,
+  className,
+}: DropdownMenuContentProps) {
+  return (
+    <RdxDropdownMenu.Portal>
+      <RdxDropdownMenu.Content
+        className={cn(
+          "rounded-2xl p-2 bg-white space-y-2 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10)] z-50",
+          "data-[side=bottom]:animate-slide-up-and-fade",
+          "data-[side=top]:animate-slide-down-and-fade",
+          className
+        )}
+      >
+        {children}
+      </RdxDropdownMenu.Content>
+    </RdxDropdownMenu.Portal>
+  );
+}
+
+interface DropdownMenuItemProps {
+  children: React.ReactNode;
+  className?: string;
+  onSelect?(): void;
+}
+
+function DropdownMenuItem({
+  children,
+  className,
+  onSelect,
+}: DropdownMenuItemProps) {
+  return (
+    <RdxDropdownMenu.Item
+      onSelect={onSelect}
+      className={cn(
+        "min-h-12 outline-none flex items-center py-2 px-4 text-sm text-gray-800 data-[highlighted]:bg-gray-50 rounded-2xl transition-colors cursor-pointer",
+        className
+      )}
+    >
+      {children}
+    </RdxDropdownMenu.Item>
+  );
+}
+
+export const DropdownMenu = {
+  Root: DropdownMenuRoot,
+  Trigger: DropdownMenuTrigger,
+  Content: DropdownMenuContent,
+  Item: DropdownMenuItem,
+};
