eeac292d758b48c3dbf8a0c7f026e536ddc98d20
juanpablow <juan.santana.macedo10@outlook.com>
2024-07-25 01:33:29 -0300

    feat: Add filter modal state management with handlers

--- frontend/src/view/pages/Dashboard/components/Transactions/useTransactionsController.ts
+++ frontend/src/view/pages/Dashboard/components/Transactions/useTransactionsController.ts
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import { useDashboard } from "../DashboardContext/useDashboard";
 
 export function useTransactionsController() {
   const { areValuesVisible } = useDashboard();
+
+  const [isFiltersModalOpen, setIsFiltersModalOpen] = useState(false);
+
+  function handleOpenFiltersModal() {
+    setIsFiltersModalOpen(true);
+  }
+
+  function handleCloseFiltersModal() {
+    setIsFiltersModalOpen(false);
+  }
 
   return {
     areValuesVisible,
     transactions: [1],
     isInitialLoading: false,
     isLoading: false,
+    isFiltersModalOpen,
+    handleOpenFiltersModal,
+    handleCloseFiltersModal,
   };
 }
