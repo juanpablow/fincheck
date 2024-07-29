e117cf5dcbe4382e4ed58aedbb90b964f3650c37
juanpablow <juan.santana.macedo10@outlook.com>
2024-07-29 16:12:12 -0300

    feat: add `Popover` component with radix-ui/react-popover - Install `radix-ui/react-popover` dependency

---
+++ frontend/src/view/components/Popover.tsx
@@ -0,0 +1,43 @@
+/* eslint-disable react-refresh/only-export-components */
+import { cn } from "@app/utils/cn";
+import * as RdxPopover from "@radix-ui/react-popover";
+
+function PopoverRoot({ children }: { children: React.ReactNode }) {
+  return <RdxPopover.Root>{children}</RdxPopover.Root>;
+}
+
+function PopoverTrigger({ children }: { children: React.ReactNode }) {
+  return (
+    <RdxPopover.Trigger className="outline-none cursor-pointer" asChild>
+      {children}
+    </RdxPopover.Trigger>
+  );
+}
+
+interface PopoverContentProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+function PopoverContent({ children, className }: PopoverContentProps) {
+  return (
+    <RdxPopover.Portal>
+      <RdxPopover.Content
+        className={cn(
+          "rounded-2xl bg-white space-y-2 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10)] z-[99] p-4",
+          "data-[side=bottom]:animate-slide-up-and-fade",
+          "data-[side=top]:animate-slide-down-and-fade",
+          className
+        )}
+      >
+        {children}
+      </RdxPopover.Content>
+    </RdxPopover.Portal>
+  );
+}
+
+export const Popover = {
+  Root: PopoverRoot,
+  Trigger: PopoverTrigger,
+  Content: PopoverContent,
+};
