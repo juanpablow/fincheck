7bccffd8823272a42a06c73697c2293e5aa396ba
juanpablow <juan.santana.macedo10@outlook.com>
2024-07-30 22:07:41 -0300

    feat: make `AccountCard` clickable for editing

--- frontend/src/view/pages/Dashboard/components/Accounts/AccountCard.tsx
+++ frontend/src/view/pages/Dashboard/components/Accounts/AccountCard.tsx
@@ -2,20 +2,21 @@ import { formatCurrency } from "@app/utils/formatCurrency";
 import { BankAccountTypeIcon } from "@view/components/icons/BankAccountTypeIcon";
 import { useDashboard } from "../DashboardContext/useDashboard";
 import { cn } from "@app/utils/cn";
+import { BankAccount } from "@app/entities/BankAccount";
 
 interface AccountCardProps {
-  color: string;
-  name: string;
-  balance: number;
-  type: "cash" | "checking" | "investment";
+  data: BankAccount;
 }
 
-export function AccountCard({ color, name, balance, type }: AccountCardProps) {
-  const { areValuesVisible } = useDashboard();
+export function AccountCard({ data }: AccountCardProps) {
+  const { color, currentBalance, name, type } = data;
+  const { areValuesVisible, openEditAccountModal } = useDashboard();
   return (
     <div
       className="p-4 bg-white rounded-2xl h-[200px] flex flex-col justify-between border-b-8 border-teal-950"
       style={{ borderColor: color }}
+      role="button"
+      onClick={() => openEditAccountModal(data)}
     >
       <div>
         <BankAccountTypeIcon type={type} />
@@ -31,7 +32,7 @@ export function AccountCard({ color, name, balance, type }: AccountCardProps) {
             !areValuesVisible && "blur-[6px]"
           )}
         >
-          {formatCurrency(balance)}
+          {formatCurrency(currentBalance)}
         </span>
         <small className="text-gray-600 text-sm">Saldo atual</small>
       </div>
