684b37b49cc9ea8d2e0b3e881dc5922357586bcb
juanpablow <juan.santana.macedo10@outlook.com>
2024-07-30 22:40:54 -0300

    feat: Enhance error handling in `signin` and `signup`

--- frontend/src/app/services/authService/signin.ts
+++ frontend/src/app/services/authService/signin.ts
@@ -1,5 +1,6 @@
 import { sleep } from "@app/utils/sleep";
 import { httpClient } from "../httpClient";
+import { AxiosError } from "axios";
 
 export interface SigninParams {
   email: string;
@@ -13,7 +14,37 @@ interface SigninResponse {
 export async function signin(body: SigninParams) {
   await sleep();
 
-  const { data } = await httpClient.post<SigninResponse>("/auth/signin", body);
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const { data } = await httpClient.post<SigninResponse>(
+      "/auth/signin",
+      body
+    );
 
-  return data;
+    return data;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      if (error.code === "ECONNABORTED") {
+        // Timeout error
+        console.error("A requisição expirou. Tente novamente mais tarde.");
+        throw new Error("A requisição expirou. Tente novamente mais tarde.");
+      } else if (error.response) {
+        // The server return a status error (ex: 4xx, 5xx)
+        console.error("Erro de requisição:", error.response.data);
+        throw new Error("Erro ao tentar fazer login.");
+      } else if (error.request) {
+        // The request was done, but dont receive responses from server
+        console.error("Sem resposta do servidor:", error.request);
+        throw new Error("Não foi possível conectar ao servidor.");
+      } else {
+        // Error during a request
+        console.error("Erro durante a requisição:", error.message);
+        throw new Error("Erro ao tentar fazer login.");
+      }
+    } else {
+      // Error not from Axios
+      console.error("Erro inesperado:", error);
+      throw new Error("Erro inesperado.");
+    }
+  }
 }
--- frontend/src/app/services/authService/signup.ts
+++ frontend/src/app/services/authService/signup.ts
@@ -1,5 +1,6 @@
 import { sleep } from "@app/utils/sleep";
 import { httpClient } from "../httpClient";
+import { AxiosError } from "axios";
 
 export interface SignupParams {
   name: string;
@@ -14,7 +15,36 @@ interface SignupResponse {
 export async function signup(body: SignupParams) {
   await sleep();
 
-  const { data } = await httpClient.post<SignupResponse>("/auth/signup", body);
+  try {
+    const { data } = await httpClient.post<SignupResponse>(
+      "/auth/signup",
+      body
+    );
 
-  return data;
+    return data;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      if (error.code === "ECONNABORTED") {
+        // Timeout error
+        console.error("A requisição expirou. Tente novamente mais tarde.");
+        throw new Error("A requisição expirou. Tente novamente mais tarde.");
+      } else if (error.response) {
+        // The server return a status error (ex: 4xx, 5xx)
+        console.error("Erro de requisição:", error.response.data);
+        throw new Error("Erro ao tentar fazer login.");
+      } else if (error.request) {
+        // The request was done, but dont receive responses from server
+        console.error("Sem resposta do servidor:", error.request);
+        throw new Error("Não foi possível conectar ao servidor.");
+      } else {
+        // Error during a request
+        console.error("Erro durante a requisição:", error.message);
+        throw new Error("Erro ao tentar fazer login.");
+      }
+    } else {
+      // Error not from Axios
+      console.error("Erro inesperado:", error);
+      throw new Error("Erro inesperado.");
+    }
+  }
 }
