bd420cc70a10d7887dc604214ac602ec758f83a8
juanpablow <juan.santana.macedo10@outlook.com>
2024-07-30 22:02:47 -0300

    feat: calculate total current balance from bank accounts

--- frontend/src/view/pages/Dashboard/components/Accounts/useAccountsController.ts
+++ frontend/src/view/pages/Dashboard/components/Accounts/useAccountsController.ts
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { useWindowWidth } from "@app/hooks/useWindowWidth";
 import { useDashboard } from "../DashboardContext/useDashboard";
+import { bankAccountsService } from "@app/services/bankAccountsService";
+import { useQuery } from "@tanstack/react-query";
 
 export function useAccountsController() {
   const windowWidth = useWindowWidth();
@@ -12,14 +14,26 @@ export function useAccountsController() {
     isEnd: false,
   });
 
+  const { data, isFetching } = useQuery({
+    queryKey: ["bankAccounts"],
+    queryFn: bankAccountsService.getAll,
+  });
+
+  const currentBalance = useMemo(() => {
+    if (!data) return 0;
+
+    return data.reduce((total, account) => total + account.currentBalance, 0);
+  }, [data]);
+
   return {
     sliderState,
-    setSliderState,
     windowWidth,
     areValuesVisible,
+    isLoading: isFetching,
+    accounts: data ?? [],
+    setSliderState,
     toggleValueVisibility,
-    isLoading: false,
-    accounts: [],
     openNewAccountModal,
+    currentBalance,
   };
 }
