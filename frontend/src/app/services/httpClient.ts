db46e3974bbbdb971aa4cdcd0e3f1360c8485fe9
juanpablow <juan.santana.macedo10@outlook.com>
2024-07-30 22:39:35 -0300

    chore: add 5 seconds timeout for `axios` response

--- frontend/src/app/services/httpClient.ts
+++ frontend/src/app/services/httpClient.ts
@@ -4,6 +4,7 @@ import { sleep } from "../utils/sleep";
 
 export const httpClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+  timeout: 5000,
 });
 
 httpClient.interceptors.request.use((config) => {
