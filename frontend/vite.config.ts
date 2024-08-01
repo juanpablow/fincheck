9c28af71e959685a35480811401ad3940e3668b6
juanpablow <juan.santana.macedo10@outlook.com>
2024-08-01 00:26:04 -0300

    chore: configure aliases in vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@app": path.resolve(__dirname, "src/app"),
+      "@view": path.resolve(__dirname, "src/view"),
+      "@router": path.resolve(__dirname, "/src/Router/"),
+      "@assets": path.resolve(__dirname, "/src/assets/"),
+    },
+  },
 });
