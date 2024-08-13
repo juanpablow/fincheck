bb0ff63853c87b363b5952eb989045212af2bdce
juanpablow <juan.santana.macedo10@outlook.com>
2024-08-13 16:23:28 -0300

    feat: generate user avatar initials from name

--- frontend/src/view/components/UserMenu.tsx
+++ frontend/src/view/components/UserMenu.tsx
@@ -3,14 +3,20 @@ import { DropdownMenu } from "./DropdownMenu";
 import { useAuth } from "@app/hooks/useAuth";
 
 export function UserMenu() {
-  const { signout } = useAuth();
+  const { signout, user } = useAuth();
+  const splittedName = user?.name.toUpperCase().split(" ");
+  if (!splittedName) return;
+  const avatarName =
+    splittedName.length > 1
+      ? `${splittedName[0].charAt(0)}${splittedName[1].charAt(0)}`
+      : splittedName[0].slice(0, 2);
 
   return (
     <DropdownMenu.Root>
       <DropdownMenu.Trigger>
         <div className="bg-teal-50 rounded-full w-12 h-12 flex items-center justify-center border border-teal-100">
           <span className="text-sm tracking-[0.5px] text-teal-900 font-medium">
-            JP
+            {avatarName}
           </span>
         </div>
       </DropdownMenu.Trigger>
