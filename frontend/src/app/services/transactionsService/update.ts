3ae393c6251d13fbb10e1dd62640d88737c3fb59
juanpablow <juan.santana.macedo10@outlook.com>
2024-08-13 16:26:50 -0300

    feat: add `update` functionality for `transactions`

---
+++ frontend/src/app/services/transactionsService/update.ts
@@ -0,0 +1,17 @@
+import { httpClient } from "../httpClient";
+
+export interface UpdateTransactionParams {
+  id: string;
+  bankAccountId: string;
+  categoryId: string;
+  name: string;
+  value: number;
+  date: string;
+  type: "income" | "expense";
+}
+
+export async function update({ id, ...params }: UpdateTransactionParams) {
+  const { data } = await httpClient.put(`/transactions/${id}`, params);
+
+  return data;
+}
