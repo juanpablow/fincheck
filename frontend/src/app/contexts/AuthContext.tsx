32f554689df4568ef55ba44ce2c125009cd430d5
juanpablow <juan.santana.macedo10@outlook.com>
2024-08-13 16:34:52 -0300

    refactor: enhance `AuthProvider` with `user` state management and query invalidation on `signout`

--- frontend/src/app/contexts/AuthContext.tsx
+++ frontend/src/app/contexts/AuthContext.tsx
@@ -1,12 +1,14 @@
 import { createContext, useCallback, useEffect, useState } from "react";
 import { localStorageKeys } from "../config/localStorageKeys";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { usersService } from "../services/usersService";
 import toast from "react-hot-toast";
 import { LaunchScreen } from "@view/components/LaunchScreen";
+import { User } from "@app/entities/User";
 
 interface AuthContextValue {
   signedIn: boolean;
+  user: User | undefined;
   signin(accessToken: string): void;
   signout(): void;
 }
@@ -14,13 +16,16 @@ interface AuthContextValue {
 export const AuthContext = createContext({} as AuthContextValue);
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [manualSignout, setManualSignout] = useState(false);
   const [signedIn, setSignedIn] = useState<boolean>(() => {
     const storedAccessToken = localStorage.getItem(
-      localStorageKeys.ACCESS_TOKEN
+      localStorageKeys.ACCESS_TOKEN,
     );
 
     return !!storedAccessToken;
   });
+
+  const queryClient = useQueryClient();
 
   const signin = useCallback((accessToken: string) => {
     localStorage.setItem(localStorageKeys.ACCESS_TOKEN, accessToken);
@@ -34,9 +39,11 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     localStorage.removeItem(localStorageKeys.ARE_VALUES_VISIBLE);
 
     setSignedIn(false);
-  }, []);
+    setManualSignout(true);
+    queryClient.invalidateQueries({ queryKey: ["users", "me"] });
+  }, [queryClient]);
 
-  const { isError, isFetching, isSuccess } = useQuery({
+  const { isError, isFetching, isSuccess, data } = useQuery({
     queryKey: ["users", "me"],
     queryFn: () => usersService.me(),
     enabled: signedIn,
@@ -44,16 +51,23 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
   });
 
   useEffect(() => {
-    if (isError) {
+    if (isError && !manualSignout) {
       toast.error("Sua sessão expirou!");
       signout();
     }
-  }, [isError, signout]);
+  }, [isError, signout, manualSignout]);
+
+  useEffect(() => {
+    if (!signedIn) {
+      setManualSignout(false);
+    }
+  }, [signedIn]);
 
   return (
     <AuthContext.Provider
       value={{
         signedIn: isSuccess && signedIn,
+        user: data?.data,
         signin,
         signout,
       }}
--- frontend/src/app/hooks/useAuth.ts
+++ frontend/src/app/hooks/useAuth.ts
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 
 export function useAuth() {
-  const { signedIn, signin, signout } = useContext(AuthContext);
+  const { signedIn, signin, signout, user } = useContext(AuthContext);
 
-  return { signedIn, signin, signout };
+  return { signedIn, signin, signout, user };
 }
