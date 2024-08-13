57f77e142f2ad8094b1e758938f6ab3a54ee2a0a
juanpablow <juan.santana.macedo10@outlook.com>
2024-08-13 16:31:20 -0300

    refactor: enhance error handling in `signup` function for clearer feedback

--- frontend/src/app/services/authService/signup.ts
+++ frontend/src/app/services/authService/signup.ts
@@ -18,32 +18,25 @@ export async function signup(body: SignupParams) {
   try {
     const { data } = await httpClient.post<SignupResponse>(
       "/auth/signup",
-      body
+      body,
     );
 
     return data;
   } catch (error) {
     if (error instanceof AxiosError) {
+      if (error.message === "Network Error") {
+        throw new Error("Server unreachable.");
+      }
       if (error.code === "ECONNABORTED") {
-        // Timeout error
-        console.error("A requisição expirou. Tente novamente mais tarde.");
         throw new Error("A requisição expirou. Tente novamente mais tarde.");
       } else if (error.response) {
-        // The server return a status error (ex: 4xx, 5xx)
-        console.error("Erro de requisição:", error.response.data);
-        throw new Error("Erro ao tentar fazer login.");
+        throw new Error(error.response.data.message);
       } else if (error.request) {
-        // The request was done, but dont receive responses from server
-        console.error("Sem resposta do servidor:", error.request);
         throw new Error("Não foi possível conectar ao servidor.");
       } else {
-        // Error during a request
-        console.error("Erro durante a requisição:", error.message);
         throw new Error("Erro ao tentar fazer login.");
       }
     } else {
-      // Error not from Axios
-      console.error("Erro inesperado:", error);
       throw new Error("Erro inesperado.");
     }
   }
